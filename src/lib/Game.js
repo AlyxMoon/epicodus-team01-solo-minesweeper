1de5f28fc9792c7118512cfde57fb417966f141d
AlyxMoon <allisterkmoon@gmail.com>
2021-07-06 15:15:36 -0700

    account for mine positions in game board creation

--- src/lib/Game.js
+++ src/lib/Game.js
@@ -1,18 +1,28 @@
 import Cell from '@/lib/Cell'
+import generateRandomPositions from './generateRandomPositions'
 
 export default class Game {
   constructor ({
     colCount = 0,
     rowCount = 0,
+    mineCount = 0,
+    minePositions = generateRandomPositions(mineCount, [0, rowCount - 1], [0, colCount - 1]),
   } = {}) {
-    this.board = this.generateBoard(rowCount, colCount)
-    this.colCount = 0
-    this.rowCount = 0
+    this.board = this.generateBoard(rowCount, colCount, minePositions)
+    this.colCount = colCount
+    this.rowCount = rowCount
+    this.mineCount = mineCount
   }
 
-  generateBoard (row = 0, col = 0) {
-    return [...Array(row)].map(() => {
-      return [...Array(col)].map(() => new Cell())
+  generateBoard (row = 0, col = 0, minePositions = []) {
+    return [...Array(row)].map((_, r) => {
+      return [...Array(col)].map((_, c) => {
+        const isMine = minePositions.some(position => {
+          return position[0] === r && position[1] === c
+        })
+
+        return new Cell({ isMine })
+      })
     })
   }
 }
--- tests/unit/lib/Game.test.js
+++ tests/unit/lib/Game.test.js
@@ -2,28 +2,36 @@ import Cell from '@/lib/Cell'
 import Game from '@/lib/Game'
 
 describe('class Game', () => {
-  const cell = new Cell()
+  const cell = (count = 0) => new Cell({ adjacentMineCount: count })
+  const mine = (count = 0) => new Cell({ isMine: true, adjacentMineCount: count })
 
   it('should be created with default values', () => {
     const game = new Game()
 
     expect(game).toEqual({
       board: [],
+      mineCount: 0,
       colCount: 0,
       rowCount: 0,
     })
   })
 
   it('should be created with default values overridden', () => {
-    const game = new Game({ rowCount: 2, colCount: 3 })
+    const game = new Game({
+      rowCount: 2,
+      colCount: 3,
+      mineCount: 1,
+      minePositions: [[0, 0]],
+    })
 
     expect(game).toEqual({
       board: [
-        [cell, cell, cell],
-        [cell, cell, cell],
+        [mine(), cell(), cell()],
+        [cell(), cell(), cell()],
       ],
-      colCount: 0,
-      rowCount: 0,
+      rowCount: 2,
+      colCount: 3,
+      mineCount: 1,
     })
   })
 
@@ -46,6 +54,16 @@ describe('class Game', () => {
       for (const row of board5x7) {
         expect(row.length).toEqual(7)
       }
+    })
+
+    it('should set the correct cells to contain a bomb', () => {
+      const game = new Game()
+
+      const board = game.generateBoard(2, 3, [[0, 0], [1, 2]])
+      expect(board).toEqual([
+        [mine(), cell(), cell()],
+        [cell(), cell(), mine()],
+      ])
     })
   })
 })
