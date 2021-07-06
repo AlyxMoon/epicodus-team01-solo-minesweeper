a7ebf343a529e4cb14bff874ddabb0d6c4250de7
AlyxMoon <allisterkmoon@gmail.com>
2021-07-06 13:39:29 -0700

    begin creating game class - added constructor - added generateBoard

--- src/lib/Game.js
+++ src/lib/Game.js
@@ -1,6 +1,18 @@
+import Cell from '@/lib/Cell'
 
 export default class Game {
-  constructor () {
-    this.board = [[]]
+  constructor ({
+    colCount = 0,
+    rowCount = 0,
+  } = {}) {
+    this.board = this.generateBoard(rowCount, colCount)
+    this.colCount = 0
+    this.rowCount = 0
+  }
+
+  generateBoard (row = 0, col = 0) {
+    return [...Array(row)].map(() => {
+      return [...Array(col)].map(() => new Cell())
+    })
   }
 }
--- tests/unit/lib/Game.test.js
+++ tests/unit/lib/Game.test.js
@@ -1,11 +1,51 @@
+import Cell from '@/lib/Cell'
 import Game from '@/lib/Game'
 
 describe('class Game', () => {
+  const cell = new Cell()
+
   it('should be created with default values', () => {
     const game = new Game()
 
     expect(game).toEqual({
-      board: [[]],
+      board: [],
+      colCount: 0,
+      rowCount: 0,
+    })
+  })
+
+  it('should be created with default values overridden', () => {
+    const game = new Game({ rowCount: 2, colCount: 3 })
+
+    expect(game).toEqual({
+      board: [
+        [cell, cell, cell],
+        [cell, cell, cell],
+      ],
+      colCount: 0,
+      rowCount: 0,
+    })
+  })
+
+  describe('method generateBoard()', () => {
+    it('should create a board with the correct row/col count with square grid', () => {
+      const game = new Game()
+
+      const board10x10 = game.generateBoard(10, 10)
+      expect(board10x10.length).toEqual(10)
+      for (const row of board10x10) {
+        expect(row.length).toEqual(10)
+      }
+    })
+
+    it('should create a board with the correct row/col count with rectangular grid', () => {
+      const game = new Game()
+
+      const board5x7 = game.generateBoard(5, 7)
+      expect(board5x7.length).toEqual(5)
+      for (const row of board5x7) {
+        expect(row.length).toEqual(7)
+      }
     })
   })
 })
