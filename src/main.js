63d53759c20b8f155e2cfe8a5c74ea081df9689e
AlyxMoon <allisterkmoon@gmail.com>
2021-07-06 16:15:19 -0700

    do a basic rendering of the game board

--- src/main.js
+++ src/main.js
@@ -1,7 +1,28 @@
 import 'purecss/build/pure-min.css'
+import '@/styles/main.scss'
 import 'regenerator-runtime'
 
+import Game from '@/lib/Game'
+
+const drawGameBoard = (game) => {
+  const elBoard = document.querySelector('#game-board')
+  let content = ''
+
+  for (const row of game.board) {
+    for (const col of row) {
+      const cellContent = col.isMine ? '.' : col.adjacentMineCount
+      content += `<div class="cell">${cellContent}</div>`
+    }
+  }
+
+  elBoard.style.gridTemplateColumns = `repeat(${game.colCount}, var(--cell-size))`
+  elBoard.innerHTML = content
+}
+
 const main = async () => {
+  const game = new Game({ colCount: 10, rowCount: 10, mineCount: 20 })
+
+  drawGameBoard(game)
 }
 
 main()
